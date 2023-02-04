4c7398695720014e2fb797c9f928a32df93138c5
your_username <sandrok246@gmail.com>
2023-02-04 23:13:16 +0400

    0

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./App.css";
+import { wordsArray } from "./data/words";
+import { useState,useEffect } from "react";
+import { time } from "console";
+
+function App() {
+  
+  const [current, setCurrent] = useState<string>(
+    wordsArray[Math.floor(Math.random() * wordsArray.length)]
+  );
+  const [value, setValue] = useState<string>('');
+  const [score, setScore] = useState<number>(0);
+  const [timer, setTimer] = useState<number>(60);
+  const [gameStatus, setGameStatus] = useState<boolean>(false);
+
+  if (current === value){
+    setScore(score + 1)
+    setCurrent(wordsArray[Math.floor(Math.random() * wordsArray.length)])
+    setValue('')
+  }
+
+  useEffect(() => {
+    let interval:any
+    if (gameStatus) {
+      interval = setInterval(() => {
+        setTimer(timer - 1);
+      }, 1000);
+    }
+    if (timer <= 0) {
+      clearInterval(interval);
+      
+    }
+    return () => clearInterval(interval);
+  }, [gameStatus, timer]);
+
+
+  return (
+    <div className="App">
+      <header>GetWord</header>
+      <h1>score: {score}</h1>
+      <div className="gameContainer">
+        <div className="timer">{timer > 0 ? timer: "Time's up!"}</div>
+        <label className="word">{current}</label>
+        <input
+          className="input"
+          placeholder={gameStatus === false? "type to start game" : ''}
+          value={value}
+          onChange={(event) => {
+            if (timer > 0){
+              setValue(event.target.value);
+              setGameStatus(true)
+            }else{
+              setGameStatus(false)
+            }
+            
+            
+          }}
+        ></input>
+        <button className="restartBtn" onClick={() => {
+          setValue('')
+          setTimer(60)
+          setScore(0)
+        }}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
